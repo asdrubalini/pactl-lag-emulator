caa28639af5a302edf65ecd1224bb699eb18f8ea
Giovanni <indirizzoemail@mail.com>
2021-03-29 22:16:18 +0200

    code refactor

--- __main__.py
+++ __main__.py
@@ -6,34 +6,41 @@ import random
 SOURCE_ID = "1"
 
 
-def mute() -> bool:
-    command = f"pactl set-source-mute {SOURCE_ID} 1"
+class Microphone:
+    def __init__(self, source_id: str):
+        self.source_id = source_id
 
-    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-    p.wait()
+    def mute(self) -> bool:
+        command = f"pactl set-source-mute {self.source_id} 1"
 
-    return p.returncode == 0
+        p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+        p.wait()
+
+        return p.returncode == 0
+
+    def unmute(self) -> bool:
+        command = f"pactl set-source-mute {self.source_id} 0"
+
+        p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+        p.wait()
+
+        return p.returncode == 0
 
 
-def unmute() -> bool:
-    command = f"pactl set-source-mute {SOURCE_ID} 0"
-
-    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-    p.wait()
-
-    return p.returncode == 0
-
-
-def main():
+def run(microphone: Microphone):
     while True:
-        mute()
+        microphone.mute()
         time.sleep(random.uniform(0.1, 0.3))
-        unmute()
+        microphone.unmute()
         time.sleep(random.uniform(0.3, 0.5))
 
+
 if __name__ == "__main__":
+    microphone = Microphone(source_id=SOURCE_ID)
+
     try:
-        main()
+        run(microphone)
+
     except KeyboardInterrupt:
-        unmute()
+        microphone.unmute()
         print("Unmuting and closing...")
